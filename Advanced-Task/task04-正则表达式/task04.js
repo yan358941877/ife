e00a2376ef7db0508c255127bf81cb4a9238d157
yan358941877 <yan358941877@163.com>
2017-04-05 15:20:23 +0800

    add 正则表达式

--- Advanced-Task/task04-正则表达式/task04.js
+++ Advanced-Task/task04-正则表达式/task04.js
@@ -1,3 +1,4 @@
+// 验证手机号
 function isPhoneNumber(phoneNumber) {
     var pattern = /^1[3587]\d{9}$/;
     if (phoneNumber.match(pattern)) {
@@ -7,6 +8,7 @@ function isPhoneNumber(phoneNumber) {
     }
 }
 
+// 判断输入的字符串是否有相邻重复单词
 function isRepeat(str) {
     var pattern = /\W(\w+)\W\1|(\w+)^\W+\1/g;
     if (str.match(pattern)) {
